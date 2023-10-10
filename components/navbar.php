9a45644bfe7566200c94286a97dca5bcb2635cfd
holiq <holiq.ibrahim376@gmail.com>
2023-10-10 18:04:56 +0700

    init template

---
+++ components/navbar.php
@@ -0,0 +1,35 @@
+<header class="black-bg mh-header mh-fixed-nav nav-scroll mh-xs-mobile-nav wow fadeInUp" id="mh-header">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg mh-nav nav-btn">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?pages=about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?pages=skills">Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?pages=experiences">Experiences</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?pages=portfolio">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?pages=contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+include_once './components/header.php';
+include_once './components/loader.php';
+include_once './components/navbar.php';
+
+if ($_GET['pages'] == 'about') {
+    include_once './components/pages/about.php';
+} elseif ($_GET['pages'] == 'service') {
+    include_once './components/pages/service.php';
+} elseif ($_GET['pages'] == 'skills') {
+    include_once './components/pages/skills.php';
+} elseif ($_GET['pages'] == 'experiences') {
+    include_once './components/pages/experiences.php';
+} elseif ($_GET['pages'] == 'portfolio') {
+    include_once './components/pages/portfolio.php';
+} elseif ($_GET['pages'] == 'contact') {
+    include_once './components/pages/contact.php';
+} else {
+    include_once './components/pages/home.php';
+}
+
+include_once './components/footer.php';
